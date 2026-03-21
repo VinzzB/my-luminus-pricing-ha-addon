e9ba0af2690d2b5cafd16f6da7a2ea61c2f13514
VinzzB <vinzz@vinzz.be>
2026-03-21 18:49:29 +0100

    Add history for pricing sensors.

--- custom_components/my_luminus_pricing/sensor.py
+++ custom_components/my_luminus_pricing/sensor.py
@@ -69,6 +69,7 @@ class LuminusBaseSensor(LuminusBaseEntity, SensorEntity):
 
 class YearlyPriceSensor(LuminusBaseSensor):
 
+    _attr_state_class  = SensorStateClass.MEASUREMENT
     _attr_device_class = SensorDeviceClass.MONETARY
     _attr_native_unit_of_measurement = 'EUR/year'
     _attr_suggested_display_precision = 2
@@ -76,6 +77,7 @@ class YearlyPriceSensor(LuminusBaseSensor):
 
 class EnergyPriceSensor(LuminusBaseSensor):
 
+    _attr_state_class  = SensorStateClass.MEASUREMENT
     _attr_device_class = SensorDeviceClass.MONETARY
     _attr_native_unit_of_measurement = 'EUR/kWh'
     _attr_suggested_display_precision = 4
