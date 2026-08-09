02c40a5b2bba4765314f7772a4bedb64b99a1e6d
VinzzB <vinzz@vinzz.be>
2026-08-09 04:19:05 +0200

    Use snake case for translation keys on sensors.

--- custom_components/my_luminus_pricing/base.py
+++ custom_components/my_luminus_pricing/base.py
@@ -18,6 +18,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from .const import DOMAIN
 from .coordinator import LuminusCoordinator
 import logging
+import re
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -48,6 +49,7 @@ class LuminusBaseEntity(CoordinatorEntity):
         self.device = device
         self.device_id = device["device_id"]
         self.parameter = parameter
+        self.translation_key = self.camel_to_snake_case(parameter)
         #self._translation_key = parameter
 
     @callback
@@ -62,9 +64,16 @@ class LuminusBaseEntity(CoordinatorEntity):
         )
         self.async_write_ha_state()
 
-    @property
-    def translation_key(self):
-        return self.parameter
+    # @property
+    # def translation_key(self):
+        # return self.camel_to_snake_case(self.parameter)
+
+    def camel_to_snake_case(self, text: str) -> str:
+        """Zet CamelCase om naar snake_case."""
+        # Voegt een underscore toe voor elke hoofdletter die volgt op een kleine letter
+        str1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
+        # Behandelt opeenvolgende hoofdletters (zoals HTTPResponse -> http_response)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', str1).lower()        
         
     @property
     def device_info(self) -> DeviceInfo:
